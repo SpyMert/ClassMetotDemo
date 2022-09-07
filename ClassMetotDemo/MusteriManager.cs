2b1dae7bb83a751b3d86839c15aed10a9a833e63
user <user@DESKTOP-QGJI9M6>
2022-09-07 22:32:25 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Add (Musteri musteri)//Müşteri class'ına ait bir müşteri parametresi yaz ayraçın içine. Örnek musteri1
+        {
+            Console.WriteLine("Adı:" + musteri.Ad + " " + "Soyadı:" + musteri.Soyad +" " + "müşteri eklendi!");//Sonra parametrede girdiğin elemanın adını soyadını bu şablona koy.
+            
+        }
+        public void Delete(Musteri musteri)// sonucun bir değer olmadığı zamanlarda void kullanırız.
+        {
+            Console.WriteLine("Adı:" + musteri.Ad + " " + "Soyadı:" + musteri.Soyad + " " + "müşteri silindi!");
+        }
+
+        public void Kimlik(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.Id + " ");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Adam";
+            musteri1.Soyad = "Adamoğlu";
+            musteri1.Id = 1;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Adama";
+            musteri2.Soyad = "Traure";
+            musteri2.Id = 2;
+            Console.WriteLine(musteri1.Ad);
+            
+            MusteriManager manager = new MusteriManager();
+            manager.Add(musteri1);
+            manager.Add(musteri2);
+
+            manager.Delete(musteri1);
+            manager.Delete(musteri2);
+
+            manager.Kimlik(musteri1);
+            manager.Kimlik(musteri2);
+        }
+         
+    }
+
+
+
+
+}
+
+
